b5b0e8ab513a0e4a495ddeafb5fbb96d1749e0c7
fpotier <frederic.potier@protonmail.com>
2021-09-27 19:43:00 +0200

    Command line arguments : argtable3 library added
    - Arguments :
      o input/output file
      o verbose
      o stage="<lexical|syntactical|semantic>"
     - File checking :
      o access()
      o stat()

--- ReducedCCompiler/src/main.c
+++ ReducedCCompiler/src/main.c
@@ -1,11 +1,32 @@
+#include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
 
 #include "token.h"
 #include "syntactic_node.h"
 #include "syntactic_analysis.h"
 #include "semantic_analysis.h"
 #include "code_generation.h"
+#include "argtable3/argtable3.h"
+
+#if defined(_WIN32) || defined(WIN32) 
+    #include <io.h>
+    #define F_OK 0
+    #define W_OK 2
+    #define R_OK 4
+    #define ACCESS(pathname, mode) _access(pathname, mode)
+
+    #define STAT(pathname, statbuf) _stat(pathname, statbuf)
+    #define STAT_S _stat
+#else
+    #include <unistd.h>
+    #define ACCESS(pathname, mode) access(pathname, mode)
+
+    #define STAT(pathname, statbuf) stat(pathname, statbuf)
+    #define STAT_S stat
+#endif
 
 // Loads the source file content as a null terminated buffer allocated dynamically
 // TO BE FREED
@@ -17,24 +38,88 @@ void test_syntactical_analysis_on_file(char* path, unsigned char optimisations);
 void test_semantic_analysis_on_file(char* path, unsigned char optimisations);
 void test_compile_file(char* path, int verbose, unsigned char optimisations);
 
-int main()
-{
-    // char valid_filepath[] = "res/valid.c";
-    // char junk_filepath[] = "res/junk.c";
-    // char mix_filepath[] = "res/mix.c";
-    // 
-    // test_lexical_analysis_on_file(valid_filepath);
-    // test_lexical_analysis_on_file(junk_filepath);
-    // test_lexical_analysis_on_file(mix_filepath);
-    
-    // test_display_syntactic_tree();
+/* global arg_xxx structs */
+struct arg_lit *verb,  *help,  *version;
+struct arg_file *output,  *file;
+struct arg_str *stage;
+struct arg_end *end;
 
-    //char simple_expression_path[] = "res/simple_expression.c";
-    char simple_expression_path[] = "res/semantic.c";
-    //test_syntactical_analysis_on_file(simple_expression_path);
-    //test_semantic_analysis_on_file(simple_expression_path);
-    test_compile_file(simple_expression_path, 1, OPTI_CONST_OPERATIONS);
-    return 0;
+int main(int argc, char* argv[])
+{
+    /* the global arg_xxx structs are initialised within the argtable */
+    void* argtable[] =
+    {
+        help    = arg_litn("h", "help", 0, 1, "display this help and exit"),
+        version = arg_litn(NULL, "version", 0, 1, "display version info and exit"),
+        verb    = arg_litn("v", "verbose", 0, 1, "verbose output"),
+        output  = arg_filen("o", "output", "file", 0, 1, "output file"),
+        file    = arg_filen(NULL, NULL, "<file>", 1, 1, "input file"),
+        stage   = arg_strn(NULL, "stage", "<lexical|syntactical|semantic>", 0, 1, "stop the compilation at this stage"),
+        end     = arg_end(20),
+    };
+
+    int  exitcode = EXIT_SUCCESS;
+    char progname[] = "rcc";
+
+    int nb_errors;
+    nb_errors = arg_parse(argc, argv, argtable);
+
+    /* special case: '--help' takes precedence over error reporting */
+    if (help->count > 0)
+    {
+        printf("Reduced C Compiler.\n\n");
+        printf("Usage: %s", progname);
+        arg_print_syntax(stdout, argtable, "\n");
+        arg_print_glossary(stdout, argtable, "  %-40s %s\n");
+    }
+    else if (nb_errors > 0)
+    {
+        /* Display the error details contained in the arg_end struct.*/
+        arg_print_errors(stdout, end, progname);
+        printf("Try '%s --help' for more information.\n", progname);
+        exitcode = EXIT_FAILURE;
+    }
+    else
+    {
+        FILE *output_file = stdout;
+
+        if (ACCESS(*(file->filename), R_OK) == -1)
+        {
+            // TODO ACCESS() doesn't fail on windows when an asked permission is denied
+            if (errno == EACCES)
+            {
+                fprintf(stderr, "%s : Permission denied\n", *(file->filename));
+                exitcode = EXIT_FAILURE;
+            }
+            else if (errno == ENOENT)
+            {
+                fprintf(stderr, "%s : No such file or directory\n", *(file->filename));
+                exitcode = EXIT_FAILURE;
+            }
+        }
+        else
+        {
+            if (output->count > 0)
+            {
+                output_file = fopen(*(output->filename), "w");
+                if (output_file == NULL)
+                {
+                    fprintf(stderr, "%s : Failed to open the output file\n", *(file->filename));
+                    exitcode = EXIT_FAILURE;
+                }
+            }
+
+            if (output_file != NULL)
+            {
+                // Stage handling
+            }
+        }
+    }
+
+    /* deallocate each non-null entry in argtable[] */
+    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+
+    return exitcode;
 }
 
 void test_compile_file(char* path, int verbose, unsigned char optimisations)
